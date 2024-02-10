9519e402b94d6eb90a88d8805cc85c86057ee558
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-02-11 00:53:13 +0530

    deleting unnecessary files

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-        await this.$connect();
-    }
+export class DatabaseService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
 }
