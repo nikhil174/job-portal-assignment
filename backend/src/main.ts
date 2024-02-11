ff0e450cef04f299a418211bc83215417214c427
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-02-12 04:50:34 +0530

    update backend port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cors());
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5004);
 }
 bootstrap();
